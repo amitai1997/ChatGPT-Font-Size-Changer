d2d7767de8bea1e4ec13eceac15df3736e517546
amitai salmon <amitai1997@gmail.com>
2023-04-16 02:06:15 +0300

    darggable side menu

--- fontSizeChanger.js
+++ fontSizeChanger.js
@@ -3,8 +3,8 @@
 // @namespace     https://chat.openai.com/
 // @version       1.2
 // @description   The "Font Size Changer with Side Pane Width Draggable" script is a Tampermonkey user script that modifies the font size and other styles of web elements with the class ".text-base". It sets the font size, line height, font family, and font weight to predefined values for improved readability. Additionally, the script adjusts the max width of elements with the class ".xl:max-w-3xl" to a specific value. Furthermore, it deactivates the "font-size" property for elements with the class ".prose" by setting it to "inherit". These changes are made using the GM_addStyle function, making it easy to customize the font styles according to your preference. The script also allows you to change the width of elements with the class ".md:w-[260px]" (side pane) by dragging it with the cursor.
-// @match         *://*/*
-// @author       Amitai Salmon
+// @match         https://chat.openai.com/*
+// @author        Amitai Salmon
 // @grant         GM_addStyle
 // ==/UserScript==
 
@@ -13,11 +13,12 @@
 
     // Define CSS values as variables
     let fontSize = 1.3;
-    let lineHeight = 2.1;
+    let lineHeight = 2.2;
     const TEXT_BASE_FONT_FAMILY = "Calibri";
     const TEXT_BASE_FONT_WEIGHT = "100";
     const XL_MAX_WIDTH = "70rem";
     const SIDE_MENU_MIN_WIDTH = 300; // minimum width of side pane in pixels
+    var editMenu = 0;
 
     // Function to change the font size and side pane width
     function changeFontSizeAndSidePaneWidth() {
@@ -49,7 +50,8 @@
 
     // Function to handle side pane width dragging
     function handleSidePaneWidthDrag(event) {
-        if (event.buttons !== 1); // exit if mouse button is not pressed
+        if (event.buttons !== 1) return; // exit if mouse button is not pressed
+        if (editMenu !== 1) return;
         const newWidth = event.clientX;
         const sidePane = document.querySelector(".md\\:w-\\[260px\\]");
         if (sidePane) {
@@ -59,29 +61,29 @@
 
     // Function to increase font size
     function increaseFontSize() {
-        fontSize += 0.1;
+        fontSize += 0.2;
         changeFontSizeAndSidePaneWidth();
     }
 
     // Function to decrease font size
     function decreaseFontSize() {
-        fontSize -= 0.1;
-        if (fontSize < 0.1) {
-            fontSize = 0.1;
+        fontSize -= 0.2;
+        if (fontSize < 0.2) {
+            fontSize = 0.2;
         }
         changeFontSizeAndSidePaneWidth();
     }
     // Function to increase line height
     function increaseLineHeight() {
-        lineHeight += 0.1;
+        lineHeight += 0.2;
         changeFontSizeAndSidePaneWidth();
     }
 
     // Function to decrease line height
     function decreaseLineHeight() {
-        lineHeight -= 0.1;
-        if (lineHeight < 0.1) {
-            lineHeight = 0.1;
+        lineHeight -= 0.2;
+        if (lineHeight < 0.2) {
+            lineHeight = 0.2;
         }
         changeFontSizeAndSidePaneWidth();
     }
@@ -105,17 +107,36 @@
     decreaseFontSizeButton.addEventListener("click", decreaseFontSize);
     decreaseFontSizeButton.addEventListener("click", decreaseLineHeight);
 
+    const editMenuButtom = document.createElement("button");
+    editMenuButtom.textContent = "edit menu";
+    editMenuButtom.style.position = "fixed";
+    editMenuButtom.style.bottom = "20px";
+    editMenuButtom.style.right = "140px";
+    editMenuButtom.style.zIndex = "9999";
+    editMenuButtom.addEventListener("click", myfunc);
+
     // Add buttons to the DOM
     document.body.appendChild(increaseFontSizeButton);
     document.body.appendChild(decreaseFontSizeButton);
+    document.body.appendChild(editMenuButtom);
 
     // Add event listener for side pane width dragging
     //window.addEventListener('dblclick', handleSidePaneWidthDrag);
 
-    window.addEventListener("click", function(evt) {
-        if (evt.detail === 3) {
-            handleSidePaneWidthDrag(evt);
+    function myfunc() {
+        if (editMenu == 0) {
+            editMenu = 1;
+            editMenuButtom.style.fontWeight = "bold";
+        } else {
+            editMenu = 0;
+            editMenuButtom.style.fontWeight = "normal";
         }
+    }
+
+    window.addEventListener("mousemove", function(evt) {
+        //if (evt.detail === 3) {
+        handleSidePaneWidthDrag(evt);
+        //}
     });
 
     // Call the initial function to apply font size and side pane width changes
